3914429461439cbc6813733d29ec5da5ea4da70c
tmatis <tmatis@student.42.fr>
2021-09-19 17:49:14 +0200

    remove ref to enum that is forbiddeb in cpp

--- tests/test_sfinae.cpp
+++ tests/test_sfinae.cpp
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 21:30:40 by tmatis            #+#    #+#             */
-/*   Updated: 2021/09/05 23:16:39 by tmatis           ###   ########.fr       */
+/*   Updated: 2021/09/19 17:48:27 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 class A {};
  
-enum E : int {};
 
 template <class T>
 typename ft::enable_if<ft::is_integral<T>::value, bool>::type
@@ -27,7 +26,6 @@ void test_sfinae()
 	module_head("sfinae");
 	std::cout << std::boolalpha;
     std::cout << ft::is_integral<A>::value << '\n';
-    std::cout << ft::is_integral<E>::value << std::endl;
     std::cout << ft::is_integral<float>::value << std::endl;
     std::cout << ft::is_integral<int>::value << std::endl;
     std::cout << ft::is_integral<const int>::value << std::endl;
